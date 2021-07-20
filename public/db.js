281a6733b7c4e2a963e4c695baa733ca9047e187
CWhirl <68519564+CWhirl@users.noreply.github.com>
2021-07-20 17:03:00 -0600

    reverted to previous commit

--- public/db.js
+++ public/db.js
@@ -8,9 +8,20 @@ var request = window.indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
     const db = request.result;
-    db.createObjectStore(storeName, { keyPath: "_id" });
+    db.createObjectStore("StoreName", { keyPath: "_id" });
 
     db.onerror = function(errorEvent) {
       console.log("Error loading database.");
     };
 };
+
+function checkDatabase() {
+    let transaction = db.transaction(["StoreName"], 'readwrite');
+    const store = transaction.objectStore("StoreName");
+    const records = store.getAll();
+
+    records.onsuccess = function () {
+
+    }
+};
+
