a01d3297edd7f0f28be457533b5370b4ca4118c6
CWhirl <68519564+CWhirl@users.noreply.github.com>
2021-07-20 13:56:58 -0600

    check if indexed db needs updated

--- public/db.js
+++ public/db.js
@@ -1,3 +1,16 @@
+let db;
+
 if (!window.indexedDB) {
     console.log("Your browser doesn't support a stable version of IndexedDB. Offline tracking will not be available.");
-}
+};
+
+var request = window.indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+    const db = request.result;
+    db.createObjectStore(storeName, { keyPath: "_id" });
+
+    db.onerror = function(errorEvent) {
+      console.log("Error loading database.");
+    };
+};
